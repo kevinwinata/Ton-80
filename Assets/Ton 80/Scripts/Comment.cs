38834900160e472c3020a5e763d9a89327ea8452
Kevin Winata <kevinwinata@gmail.com>
2015-04-24 20:44:30 +0700

    Adding comments (tentative)

--- Assets/Ton 80/Scripts/Comment.cs
+++ Assets/Ton 80/Scripts/Comment.cs
@@ -5,6 +5,7 @@ using Pose = Thalmic.Myo.Pose;
 public class Comment : MonoBehaviour
 {
 	public GameObject myo = null;
+	public ScoreBoard scoreBoard;
 
 	void OnGUI ()
 	{
@@ -60,6 +61,91 @@ public class Comment : MonoBehaviour
 					break;
 			}
 		}
+
+		string comment;
+		switch(scoreBoard.commentIdx) 
+		{
+			case 0 : 
+				switch(Random.Range(0,6)) 
+				{
+					case 0 : 
+						comment = "What about you join the Stormtroopers?";
+						break;
+					case 1 : 
+						comment = "wow  much accurate  such skill  many score";
+						break;
+					case 2 : 
+						comment = "Y U No hit";
+						break;
+					case 3 : 
+						comment = "Woah! You just killed a mosquito!";
+						break;
+					case 4 : 
+						comment = "Should I bring that dartboard closer to your face?";
+						break;
+					case 5 : 
+						comment = "You had one job, man. One job.";
+						break;
+					case 6 : 
+						comment = "So close, yet so far.";
+						break;
+				}
+				break;
+			case 1 :
+				switch(Random.Range(0,1)) 
+				{
+					case 0 : 
+						comment = "It's a bird! It's a plane! No, IT'S THE DART YOU THREW";
+						break;
+					case 1 : 
+						comment = "The force is strong in this one.";
+						break;
+				}
+				break;
+			case 2 :
+				switch(Random.Range(0,3)) 
+				{
+					case 0 : 
+						comment = "";
+						break;
+					case 1 : 
+						comment = "";
+						break;
+					case 2 : 
+						comment = "";
+						break;
+					case 3 : 
+						comment = "";
+						break;
+				}
+				break;
+			case 3 :
+				switch(Random.Range(0,1)) 
+				{
+					case 0 : 
+						comment = "Rheumatics? At your age?";
+						break;
+					case 1 : 
+						comment = "Do you even lift bro?";
+						break;
+				}
+				break;
+			case 4 :
+				switch(Random.Range(0,2)) 
+				{
+					case 0 : 
+						comment = "Keep Calm and Wipe Your Hands";
+						break;
+					case 1 : 
+						comment = "WATCH YOUR FEET, DART INCOMING";
+						break;
+					case 2 : 
+						comment = "Well, I guess the chance is just.. slipped out of your hands. YEEAAAAHH";
+						break;
+				}
+				break;
+		}
+		GUI.Label(new Rect(12,Screen.height-20,Screen.width,Screen.height),comment);
 	}
 
 	void Update ()
--- Assets/Ton 80/Scripts/ScoreBoard.cs
+++ Assets/Ton 80/Scripts/ScoreBoard.cs
@@ -7,6 +7,7 @@ public class ScoreBoard : MonoBehaviour
 	public int maxDart;
 	public int curDart;
 	public Vector3[] dartPositions;
+	public int commentIdx;
 
 	void Start() 
 	{
@@ -35,64 +36,94 @@ public class ScoreBoard : MonoBehaviour
 
 	public int countScores(Vector3 position)
 	{
-		if (transform.position.y < -2)
-			return 0;
-			
-		Vector3 v = transform.position - position;
-		double radius = Mathf.Sqrt(v.x*v.x + v.y*v.y);
-		double angle = Mathf.Atan2(v.y, v.x);
+		int result = 0;
+		if (transform.position.y > 2) 
+		{
+			commentIdx = 1;
+		}
+		else if (transform.position.x < -5) 
+		{
+			commentIdx = 2;
+		}
+		else if (transform.position.y < -3) 
+		{
+			commentIdx = 3;
+		}
+		else if (transform.position.z < 1) 
+		{
+			commentIdx = 4;
+		}
+		else
+		{
+			Vector3 v = transform.position - position;
+			double radius = Mathf.Sqrt(v.x*v.x + v.y*v.y);
+			double angle = Mathf.Atan2(v.y, v.x);
 
-		int score = 0;
-		if 		(angle <= Mathf.PI/10*1.5 && angle > Mathf.PI/10*0.5)
-			score = 13;
-		else if (angle <= Mathf.PI/10*2.5 && angle > Mathf.PI/10*1.5)
-			score = 4;
-		else if (angle <= Mathf.PI/10*3.5 && angle > Mathf.PI/10*2.5)
-			score = 18;
-		else if (angle <= Mathf.PI/10*4.5 && angle > Mathf.PI/10*3.5)
-			score = 1;
-		else if (angle <= Mathf.PI/10*5.5 && angle > Mathf.PI/10*4.5)
-			score = 20;
-		else if (angle <= Mathf.PI/10*6.5 && angle > Mathf.PI/10*5.5)
-			score = 5;
-		else if (angle <= Mathf.PI/10*7.5 && angle > Mathf.PI/10*6.5)
-			score = 12;
-		else if (angle <= Mathf.PI/10*8.5 && angle > Mathf.PI/10*7.5)
-			score = 9;
-		else if (angle <= Mathf.PI/10*9.5 && angle > Mathf.PI/10*8.5)
-			score = 14;
-		else if (angle >= -Mathf.PI/10*1.5 && angle < -Mathf.PI/10*0.5)
-			score = 10;
-		else if (angle >= -Mathf.PI/10*2.5 && angle < -Mathf.PI/10*1.5)
-			score = 15;
-		else if (angle >= -Mathf.PI/10*3.5 && angle < -Mathf.PI/10*2.5)
-			score = 2;
-		else if (angle >= -Mathf.PI/10*4.5 && angle < -Mathf.PI/10*3.5)
-			score = 17;
-		else if (angle >= -Mathf.PI/10*5.5 && angle < -Mathf.PI/10*4.5)
-			score = 3;
-		else if (angle >= -Mathf.PI/10*6.5 && angle < -Mathf.PI/10*5.5)
-			score = 19;
-		else if (angle >= -Mathf.PI/10*7.5 && angle < -Mathf.PI/10*6.5)
-			score = 7;
-		else if (angle >= -Mathf.PI/10*8.5 && angle < -Mathf.PI/10*7.5)
-			score = 16;
-		else if (angle >= -Mathf.PI/10*9.5 && angle < -Mathf.PI/10*8.5)
-			score = 8;
-		else if (angle <= Mathf.PI/10*0.5 || angle > -Mathf.PI/10*0.5)
-			score = 6;
-		else if (angle <= Mathf.PI/10*9.5 || angle > -Mathf.PI/10*9.5)
-			score = 11;
+			int score = 0;
+			if 		(angle <= Mathf.PI/10*1.5 && angle > Mathf.PI/10*0.5)
+				score = 13;
+			else if (angle <= Mathf.PI/10*2.5 && angle > Mathf.PI/10*1.5)
+				score = 4;
+			else if (angle <= Mathf.PI/10*3.5 && angle > Mathf.PI/10*2.5)
+				score = 18;
+			else if (angle <= Mathf.PI/10*4.5 && angle > Mathf.PI/10*3.5)
+				score = 1;
+			else if (angle <= Mathf.PI/10*5.5 && angle > Mathf.PI/10*4.5)
+				score = 20;
+			else if (angle <= Mathf.PI/10*6.5 && angle > Mathf.PI/10*5.5)
+				score = 5;
+			else if (angle <= Mathf.PI/10*7.5 && angle > Mathf.PI/10*6.5)
+				score = 12;
+			else if (angle <= Mathf.PI/10*8.5 && angle > Mathf.PI/10*7.5)
+				score = 9;
+			else if (angle <= Mathf.PI/10*9.5 && angle > Mathf.PI/10*8.5)
+				score = 14;
+			else if (angle >= -Mathf.PI/10*1.5 && angle < -Mathf.PI/10*0.5)
+				score = 10;
+			else if (angle >= -Mathf.PI/10*2.5 && angle < -Mathf.PI/10*1.5)
+				score = 15;
+			else if (angle >= -Mathf.PI/10*3.5 && angle < -Mathf.PI/10*2.5)
+				score = 2;
+			else if (angle >= -Mathf.PI/10*4.5 && angle < -Mathf.PI/10*3.5)
+				score = 17;
+			else if (angle >= -Mathf.PI/10*5.5 && angle < -Mathf.PI/10*4.5)
+				score = 3;
+			else if (angle >= -Mathf.PI/10*6.5 && angle < -Mathf.PI/10*5.5)
+				score = 19;
+			else if (angle >= -Mathf.PI/10*7.5 && angle < -Mathf.PI/10*6.5)
+				score = 7;
+			else if (angle >= -Mathf.PI/10*8.5 && angle < -Mathf.PI/10*7.5)
+				score = 16;
+			else if (angle >= -Mathf.PI/10*9.5 && angle < -Mathf.PI/10*8.5)
+				score = 8;
+			else if (angle <= Mathf.PI/10*0.5 || angle > -Mathf.PI/10*0.5)
+				score = 6;
+			else if (angle <= Mathf.PI/10*9.5 || angle > -Mathf.PI/10*9.5)
+				score = 11;
 
-		int multiplier = 1;
-		if (radius > 0.5 && radius < 0.55)
-			multiplier = 2;
-		else if (radius > 0.8 && radius < 0.85)
-			multiplier = 3;
-		else if (radius > 0.85)
-			multiplier = 0;
+			int multiplier = 1;
+			if (radius > 0.5 && radius < 0.55) 
+			{
+				multiplier = 2;
+			}
+			else if (radius > 0.8 && radius < 0.85) 
+			{
+				multiplier = 3;
+			}
+			else if (radius > 0.85) 
+			{
+				multiplier = 0;
+				commentIdx = 0;
+			}
 
-		return score*multiplier;
+			result = score*multiplier;
+
+			if (result == 0) 
+				commentIdx = 6;
+			else if (result < 10) 
+				commentIdx = 5;
+		}
+		return result;
 	}
 
 	public void addDart(Vector3 position) 
