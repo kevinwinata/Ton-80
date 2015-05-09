0d385622cac5e5998773475678583d11be38c206
Kevin Winata <kevinwinata@gmail.com>
2015-05-09 14:35:58 +0700

    Start menu completed

--- Assets/Ton 80/Scripts/UIScript.cs
+++ Assets/Ton 80/Scripts/UIScript.cs
@@ -3,8 +3,23 @@ using System.Collections;
 
 public class UIScript : MonoBehaviour {
 
+	public Animator contentPanel;
+
 	public void StartGame()
 	{
 		Application.LoadLevel("Main");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void ToggleMenu()
+	{
+		contentPanel.enabled = true;
+
+		bool isHidden = contentPanel.GetBool("isHidden");
+		contentPanel.SetBool("isHidden", !isHidden);
 	}
 }
